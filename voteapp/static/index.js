09c95609400decc87ae2cdf722b9a1d07bfea005
sanjanapanwar <sanjanapanwar6@gmail.com>
2020-07-27 00:11:12 +0530

    add vote app

---
+++ voteapp/static/index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+    socket.on('connect',()=>{
+
+        document.querySelectorAll('button').forEach(button=>{
+            button.onclick =()=>{
+                const selection=button.dataset.vote;
+                socket.emit("submit vote",{"selection":selection});
+
+            };
+
+        });
+    });
+    socket.on('vote totals', data => {
+        document.querySelector('#yes').innerHTML = data.yes;
+        document.querySelector('#no').innerHTML = data.no;
+        document.querySelector('#maybe').innerHTML = data.maybe;
+    });
+  
+
+});
